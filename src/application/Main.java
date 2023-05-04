debd8fb1299a2ac9820d88f23f04c1611cf6ff7a
Rafael Moraes <rafael.msantos@sptech.school>
2023-05-04 11:48:43 -0300

    about view

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,6 @@
 package application;
-import java.io.IOException;
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,7 +8,9 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
+	private static Scene mainScene;
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -18,7 +20,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -26,6 +28,11 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
+
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
